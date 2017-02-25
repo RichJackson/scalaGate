7866d3010dd9b22452089e2c7babb38ae9e0e599
Richard Jackson <richgjackson@gmail.com>
2017-02-25 00:18:09 +0000

    added queueing

--- src/rj/Demo.scala
+++ src/rj/Demo.scala
@@ -4,14 +4,13 @@ package rj
   * Created by rich on 24/02/17.
   */
 import java.io.File
-import GateRunner._
 import gate.Gate
+import GateRunner._
 
 object Demo {
   def main (args: Array[String]){
     Gate.setGateHome(new File("/home/rich/GATE_Developer_8.3"))
     Gate.init
-
     testGateAnonymous
     testGateImplicit
   }
--- src/rj/GateRunner.scala
+++ src/rj/GateRunner.scala
@@ -1,48 +1,67 @@
 package rj
 
 import java.io.File
+import java.util.concurrent.LinkedBlockingQueue
 
-import gate.{Factory, Gate}
+import gate.{Factory, Gate, Resource}
 import gate.creole.{ANNIEConstants, ConditionalSerialAnalyserController, SerialAnalyserController}
 import gate.util.persistence.PersistenceManager
+
+
 
 /**
   * Created by rich on 24/02/17.
   */
 
-object GateRunner {
+object GateRunner  {
   Gate.getCreoleRegister().registerDirectories(new File(
-     Gate.getPluginsHome(), "ANNIE").toURI().toURL())
+    Gate.getPluginsHome(), "ANNIE").toURI().toURL())
 
-  val annieController: ConditionalSerialAnalyserController = PersistenceManager.loadObjectFromFile(
+  var annieController: ConditionalSerialAnalyserController = PersistenceManager.loadObjectFromFile(
     new File(new File(Gate.getPluginsHome, ANNIEConstants.PLUGIN_DIR), ANNIEConstants.DEFAULT_FILE))
     .asInstanceOf[ConditionalSerialAnalyserController]
-
   annieController.setCorpus(Factory.newCorpus("corpus"))
 
+  val queue = new LinkedBlockingQueue[ConditionalSerialAnalyserController](3)
+  queue.put(annieController)
 
-  implicit class runAppImplicit (s: String){
+  for (x <- 1 to 2) {
+    annieController = Factory.duplicate(annieController).asInstanceOf[ConditionalSerialAnalyserController]
+    annieController.setCorpus(Factory.newCorpus("corpus"))
+    queue.put(annieController)
+  }
+
+
+  def runApp (s:String): String ={
+    val doc = Factory.newDocument(s)
+    val controller =queue.take()
+    controller.getCorpus().add(doc)
+    controller.execute()
+    controller.getCorpus.clear()
+    val returnStr = doc.toXml()
+    Factory.deleteResource(doc)
+    queue.put(controller)
+    return returnStr
+  }
+
+
+
+
+  implicit class runAppImplicit(s: String) {
     def run: String = {
-      var doc = Factory.newDocument(s)
-      annieController.getCorpus().add(doc)
-      annieController.execute()
-      annieController.getCorpus.clear()
-      var returnStr = doc.toXml()
+      val doc = Factory.newDocument(s)
+      val controller = queue.take()
+      controller.getCorpus().add(doc)
+      controller.execute()
+      controller.getCorpus.clear()
+      val returnStr = doc.toXml()
       Factory.deleteResource(doc)
+      queue.put(controller)
       return returnStr
     }
 
   }
 
-   def runApp (s:String): String ={
-    val doc = Factory.newDocument(s)
-    annieController.getCorpus().add(doc)
-    annieController.execute()
-    annieController.getCorpus.clear()
-    var returnStr = doc.toXml()
-    Factory.deleteResource(doc)
-    return returnStr
-  }
 
   var xmlProcessor = (s:String) => runApp(s)
 
